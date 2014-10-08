d76b1839635a445da6773b5cd33fe4156c2014a5
yusuke <21311047yu@tama.ac.jp>
2014-10-08 15:43:38 +0900

    3回目のプログラムの追加

---
+++ name.java
@@ -0,0 +1,15 @@
+class name
+{
+	  public static void main(String argv[])
+	  {
+	    String name;
+        System.out.println("‚ ‚È‚½‚Ì–¼‘O‚ÍH");
+        System.out.print("name=");
+		name = new java.util.Scanner(System.in).nextLine();
+		
+		System.out.println(name+"‚³‚ñA‚±‚ñ‚É‚¿‚Í");
+		
+		
+		
+	   }
+}	     
