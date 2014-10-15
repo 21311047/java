df4b37162b0281db6f8b5c63f460108459c77fa7
yusuke <21311047yu@tama.ac.jp>
2014-10-15 16:11:46 +0900

    4回目

---
+++ keisan.java
@@ -0,0 +1,27 @@
+class keisan{
+ public static void main(String args[])
+ {
+//2‚Â‚Ì”š‚ğ“ü—Í
+int in0 = new java.util.Scanner(System.in).nextInt();
+int in1 = new java.util.Scanner(System.in).nextInt();
+System.out.println(in0+","+in1);
+
+//ˆ— ‘«‚µZ
+int add = in0+in1;
+//ˆ— ˆø‚«Z
+int sub = in0-in1;
+//ˆ— Š|‚¯Z
+int mul = in0*in1;
+//ˆ— Š„‚èZ
+int div = in0/in1;
+//ˆ— ‚ ‚Ü‚è
+int mod = in0%in1;
+//ŒvZŒ‹‰Ê
+System.out.println("‘«‚µZ"+add);
+System.out.println("ˆø‚«Z"+sub);
+System.out.println("Š|‚¯Z"+mul);
+System.out.println("Š„‚èZ"+div);
+System.out.println("‚ ‚Ü‚è"+mod);
+
+}
+          }
