745d27061fab3dd29cab6035a29fda3788c9f9b1
yusuke <21311047yu@tama.ac.jp>
2014-11-12 16:18:42 +0900

    フィボナッチ数列

---
+++ suuretu.java
@@ -0,0 +1,22 @@
+class suuretu{
+public static void main(string args[])
+{
+int [] f = new int[100];
+
+int goukei =0;
+for (int i=0 ; i<=10 + i++){
+if i == 0{
+f[0] = 0;
+System.out.println{f[0]};
+}
+if i == 1{
+f[1] = 1;
+System.out.println(f[1]);
+}
+if i >= 2{
+f[i+2] = f[i] + f[i+1];
+System.out.println(f[i]);
+}
+}
+}
+}
