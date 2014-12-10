c1a4e77521ce42897113eb6b573ed4a94b1e2d40
yusuke <21311047yu@tama.ac.jp>
2014-12-10 16:06:27 +0900

    乱数生成とリュカ数列

--- suuretulucas.java
+++ suuretulucas.java
@@ -1,16 +1,17 @@
 //リュカ数列 オーバーフロー java→BigIntegerなるキーワード発見→java BigInteger 使い方　で検索
+import java.math.*;
+
 class suuretulucas{
 public static void main(String args[])
 {
-double luca = 0;
-double [] hai = new double[101];
-hai[0] = 2;
-hai[1] = 1;
-System.out.println(hai[0]);
-System.out.println(hai[1]);
+BigInteger luca =  new BigInteger("0");
+BigInteger [] hai = new BigInteger[101];
+hai[0] = new BigInteger("2");
+hai[1] = new BigInteger("1");
+luca  = luca.add(hai[1]);
 for (int i=2 ; i<=100 ; i++){
-hai[i] = hai[i-1] + hai[i-2];
-luca  = luca + hai[i];
+hai[i] = hai[i-1].add(hai[i-2]);
+luca  = luca.add(hai[i]);
 System.out.println(luca);
 }
 }
